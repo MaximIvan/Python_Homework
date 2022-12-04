81b0ac7e1c271efc0f199effc395246fb47f0b9a
Maksim <homeice@mail.ru>
2022-12-05 01:08:42 +0300

    Задача 5 решена

--- sem3/Task_5.py
+++ sem3/Task_5.py
@@ -3,15 +3,15 @@
 # Пример:
 # для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
 
-# n = int(input('Введите размер списка чисел Фибоначчи: '))
-# list = [-n, n]
-# num1 = num2 = 1
+n = int(input('Введите размер списка чисел Фибоначчи: '))
 
-# for i in range(0, n):
-#     num1,num2 = num2, num1 + num2
-#     list.append(num2, num1)
-# for i in range(-n, 1):
-#     num1,num2 = num2, num1 - num2 
-#     list.append(num2, num1)
-# print(list)
-....
+num1 = num2 = 1
+list = [num1, num2]
+for i in range(2, n):
+    num1,num2 = num2, num1 + num2
+    list.append(num2)
+num1 = num2 = 1
+for i in range(-n, 1):
+    num1,num2 = num2, num1 - num2 
+    list.insert(0, num2)
+print(list)
