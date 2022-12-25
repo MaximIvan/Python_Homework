54dd8ee804fc375b9ba58ca6eaa0aaa657593234
Maksim <homeice@mail.ru>
2022-12-25 06:45:39 +0300

    Написал бота по загрузку видео с youtube

---
+++ sem9_bot/venv/main.py
@@ -0,0 +1,41 @@
+# Бот по загрузки видео с youtube
+
+import os
+
+from aiogram import *
+from config import *
+from pytube import YouTube
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    chat_id = message.chat.id
+    await bot.send_message(chat_id, 'Привет, отправь мне ссылку на видео, которое надо скачать\n\
+    c YouTube')
+
+@dp.message_handler()
+async def text_message(message: types.Message):
+    chat_id = message.chat.id
+    url = message.text
+    yt = YouTube(url)
+    if message.text.startswith == 'https://www.youtube.com/' or 'https://youtu.be/':
+        await bot.send_message(chat_id, f'*Начинаю загрузку видео* : *{yt.title}*\n'
+                                        f'*С канала *: [{yt.author}]({yt.channel_url})', parse_mode = 'Markdown')
+        await download_youtube_video(url, message, bot)
+
+async def download_youtube_video(url, message, bot):
+    yt = YouTube(url)
+    stream = yt.streams.filter(progressive = True, file_extension = 'mp4')
+    stream.get_highest_resolution().download(f'{message.chat.id}', f'{message.chat.id}_{yt.title}')
+    with open(f'{message.chat.id}/{message.chat.id}_{yt.title}', 'rb') as video:
+        await bot.send_video(message.chat.id, video, caption='*Приятного просмотра *', parse_mode = 'Markdown')
+        os.remove(f'{message.chat.id}/{message.chat.id}_{yt.title}')
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
+
+
+
